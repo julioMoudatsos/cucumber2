f4872ef732c67b48e7b5429e85a182181b476f71
JulioMoudatsos <juliomoud@gmail.com>
2022-10-24 01:25:13 -0300

    test

--- app/src/test/java/testandoapis/app/support/config/ServerConfig.java
+++ app/src/test/java/testandoapis/app/support/config/ServerConfig.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.Config;
 
 import javax.security.auth.login.Configuration;
 
-@Config.Sources({"classpath:properties/local.properties","classpath:properties/${ENV}.properties"})
+@Config.Sources({"classpath:properties/${ENV}.properties"})
 public interface ServerConfig extends Config {
 
     @Key("api.urlBase")
